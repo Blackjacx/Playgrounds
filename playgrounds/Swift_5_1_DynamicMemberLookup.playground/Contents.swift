1c5af5b9564d61486b9d087a9b70a44bd73da48d
Stefan Herold <stefan.herold@gmail.com>
2019-06-28 11:09:50 +0200

    Update dynamic member lookup

--- playgrounds/Swift_5_1_DynamicMemberLookup.playground/Contents.swift
+++ playgrounds/Swift_5_1_DynamicMemberLookup.playground/Contents.swift
@@ -9,17 +9,26 @@ struct Common: Equatable {
     let version: Int
 }
 
-@dynamicMemberLookup
-struct User: Equatable {
+protocol CommonProtocol {
+    var common: Common { get }
+}
 
-    let firstName: String
-    let lastName: String
-    let common: Common
-
+extension CommonProtocol {
     public subscript<T> (dynamicMember keyPath: KeyPath<Common, T>) -> T {
         common[keyPath: keyPath]
     }
 }
 
+@dynamicMemberLookup
+struct User: Equatable, CommonProtocol {
+
+    let firstName: String
+    let lastName: String
+    let common: Common
+}
+
 let common = Common(id: "", type: "", createdAt: Date(), updatedAt: Date(), version: 0)
 let user = User(firstName: "John", lastName: "Doe", common: common)
+
+
+user.createdAt
